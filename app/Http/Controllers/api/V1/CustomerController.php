c399d6297e2e4e2c5e710df340b1fe2d9ea621b7
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-21 19:28:21 -0300

    feat: add UpdateCustomerRequest

--- app/Http/Controllers/api/V1/CustomerController.php
+++ app/Http/Controllers/api/V1/CustomerController.php
@@ -83,7 +83,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
