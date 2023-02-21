8aec68a4529c5ee20b57c6d93f74e6cf2d442bb4
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-21 11:02:56 -0300

    feat: add controllers

---
+++ app/Http/Controllers/api/V1/CustomerController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index()
+    {
+        return Customer::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @return RedirectResponse
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     * @return Response
+     */
+    public function show(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @return Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @return RedirectResponse
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @return RedirectResponse
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/api/V1/InvoiceController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Invoice;
+use App\Http\Requests\StoreInvoiceRequest;
+use App\Http\Requests\UpdateInvoiceRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @return RedirectResponse
+     */
+    public function store(StoreInvoiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     * @return Response
+     */
+    public function show(Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @return Response
+     */
+    public function edit(Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @return RedirectResponse
+     */
+    public function update(UpdateInvoiceRequest $request, Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @return RedirectResponse
+     */
+    public function destroy(Invoice $invoice)
+    {
+        //
+    }
+}
